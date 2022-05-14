15fa198833462ce03a738fdb039abd6c9797f56a
archiekramer <houlbertmaxime@gmail.com>
2022-05-14 08:03:20 +0200

    test unit organization

--- tests/test_comparaisonRepertoireBDD.py
+++ tests/test_comparaisonRepertoireBDD.py
@@ -1,9 +1,18 @@
-import unittest
 
+from importlib.resources import path
+import unittest, os, sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+print(os.path.join(os.path.dirname(__file__), '..'))
+
+
+from classes.ComparaisonRepertoireBdd import ComparationRepertoireBdd
 
 class TestComparaisonRepertoire(unittest.TestCase):
 
     def setUp(self) -> None:
+        pass
 
 
     def test_ligne_repertoire(self):
